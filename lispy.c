b4c717c40464b70d974eaded2b3a7acee9278835
Pierre Gergis <pierre.s.gergis@gmail.com>
2018-07-15 18:44:48 -0400

    Working function definition capability

--- lispy.c
+++ lispy.c
@@ -280,7 +280,6 @@ lval *lval_copy(lval *v) {
         case LVAL_FUN:
             if(v->builtin) {
                 x->builtin = v->builtin;
-                break;
             } else {
                 x->builtin = NULL;
                 x->env = lenv_copy(v->env);
@@ -685,6 +684,95 @@ void lenv_add_builtins(lenv *e) {
     lenv_add_builtin(e, "\\", builtin_lambda);
 }
 
+lval *lval_call(lenv *e, lval *f, lval *a) {
+
+    /* If Builtin then simply apply that */
+    if(f->builtin) { return f->builtin(e, a); };
+
+    /* Record Argument Counts */
+    int given = a->count;
+    int total = f->formals->count;
+
+    /* While arguments still remain to be processed */
+    while(a->count) {
+
+        /* If we've run out of formal arguments to bind */
+        if(f->formals->count == 0) {
+            lval_del(a); return lval_err(
+                "Function passed too many arguments. "
+                "Got %i, Expected %i.", given, total);
+        }
+
+        /* Pop the first symbol from the formals */
+        lval *sym = lval_pop(f->formals, 0);
+
+        /* Special case to deal with '&' */
+        if(strcmp(sym->sym, "&") == 0) {
+
+            /* Ensure '&' is followed by another symbol */
+            if(f->formals->count != 1) {
+                lval_del(a);
+                return lval_err("Function format invalid. "
+                                "Symbol '&' not followed by single symbol.");
+            }
+
+            /* Next formal should be bound to remaining arguments */
+            lval *nsym = lval_pop(f->formals, 0);
+            lenv_put(f->env, nsym, builtin_list(e, a));
+            lval_del(sym); lval_del(nsym);
+            break;
+        }
+
+        /* Pop the next argument from the list */
+        lval *val = lval_pop(a, 0);
+
+        /* Bind a copy into the functions environment */
+        lenv_put(f->env, sym, val);
+
+        /* Delete symbol and value */
+        lval_del(sym); lval_del(val);
+    }
+
+    /* Argument list is now bound so can be cleaned up */
+    lval_del(a);
+
+    /* If '&' remains in formal list, bind to empty list */
+    if(f->formals->count > 0 &&
+       strcmp(f->formals->cell[0]->sym, "&") == 0) {
+
+        /* Check to ensure that & is not passed invalidly */
+        if(f->formals->count != 2) {
+            return lval_err("Function format invalid. "
+                            "Symbol '&' not followed by single symbol.");
+        }
+
+        /* Pop and delete '&' symbol */
+        lval_del(lval_pop(f->formals, 0));
+
+        /* Pop next symbol and create empty list */
+        lval *sym = lval_pop(f->formals, 0);
+        lval *val = lval_qexpr();
+
+        /* Bind to environment and delete */
+        lenv_put(f->env, sym, val);
+        lval_del(sym); lval_del(val);
+    }
+
+    /* If all formals have been bound, evaluate */
+    if(f->formals->count == 0) {
+
+        /* Set environment parent to evaluation environment */
+        f->env->par = e;
+
+        /* Evaluate and return */
+        return builtin_eval(
+            f->env, lval_add(lval_sexpr(), lval_copy(f->body)));
+    } else {
+        /* Otherwise return partially evaluated function */
+        return lval_copy(f);
+    }
+}
+
 lval *lval_eval_sexpr(lenv *e, lval *v){
 
     /* Evaluate Children */
@@ -701,17 +789,20 @@ lval *lval_eval_sexpr(lenv *e, lval *v){
     if (v->count == 0) { return v; }
 
     /* Single Expression */
-    if (v->count == 1) { return lval_take(v, 0); }
+    if (v->count == 1) { return lval_eval(e, lval_take(v, 0)); }
 
     /* Ensure First Element is a function after evaluation */
     lval *f = lval_pop(v, 0);
-    if (f->type != LVAL_FUN) {
+    if(f->type != LVAL_FUN) {
+        lval *err = lval_err(
+            "S-Expression starts with incorrect type. "
+            "Got %s, Expected %s.",
+            ltype_name(f->type), ltype_name(LVAL_FUN));
         lval_del(f); lval_del(v);
-        return lval_err("S-expression does not start with symbol!");
+        return err;
     }
 
-    /* Call builtin with operator */
-    lval *result = f->builtin(e, v);
+    lval *result = lval_call(e, f, v);
     lval_del(f);
     return result;
 }
